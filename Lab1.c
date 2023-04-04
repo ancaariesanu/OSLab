9af9b0c8a4ed85259afed9d45c09697b23dcf332
ancaariesanu <ancaariesanu@yahoo.com>
2023-04-04 09:38:37 +0300

    Partial Code -Week 7

--- Lab1.c
+++ Lab1.c
@@ -1,8 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #define MAX_PATH_LENGHT 1024
 
-void print_menu_regfile(const char* path){
+void print_menu_regfile(const char *path, struct stat st){
+    char mode;
+    do{
+        printf("Choose what option do you want:\n n-file name\n l-delete link\n d-size of the link\n t-size of the target\n a-access rights\n ");
+        scanf("%s", &mode);
+    }while(strcmp(mode, "n")==0 || strcmp(mode, "l")==0 || strcmp(mode, "d")==0 || strcmp(mode, "t")==0 || strcmp(mode, "n")==0);
+    switch (mode){
+    case 'n':
+        printf("You choose to print the link name\n");
+        printf("Name: %s", path);
+        break;
+    
+    case 'l':
+       printf("You choose to delete link\n");
+       printf("%d",st.st_size)
+       break;
+
+    case 'd':
+        printf("You choose to print the size of the link\n");
+        printf("%d",st.st_nlink)
+        break;
+
+    case 't': 
+        printf("You choose to print the size of the target\n");
+        printf("%d",st.st_mtime)
+        break;
+
+    case 'a':
+        printf("You choose to print the access rights\n");
+        printf("User:\n")
+        if(stat.st_mode & S_IRUSR){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWUSR){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXUSR){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
+
+        printf("Group:\n")
+        if(stat.st_mode & S_IRGRP){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWGRP){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXGRP){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
+
+        printf("Other:\n")
+        if(stat.st_mode & S_S_IROTH){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWOTH){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXOTH){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+void print_menu_regfile(const char *path, struct stat st){
     char mode;
     do{
         printf("Choose what option do you want:\n n-file name\n d-dimension/size\n h-number of hard links\n m-time of last modification\n a-access rights\n l-create a symbolic link\n");
@@ -10,22 +108,92 @@ void print_menu_regfile(const char* path){
     }while(strcmp(mode, "n")==0 || strcmp(mode, "d")==0 || strcmp(mode, "h")==0 || strcmp(mode, "m")==0 || strcmp(mode, "a")==0 || strcmp(mode, "l")==0 );
     switch (mode){
     case 'n':
-        printf("You choose to print the name of the regular file %s", path);
+        printf("You choose to print the name of the regular file\n");
+        printf("Name: %s", path);
         break;
     
     case 'd':
-        break;
+       printf("You choose to print the size of the regular file\n");
+       printf("%d",st.st_size)
+       break;
 
     case 'h':
+        printf("You choose to print the number of the hard links of the regular file\n");
+        printf("%d",st.st_nlink)
         break;
 
-    case 'm':
+    case 'm': 
+        printf("You choose to print the last time of modification of the regular file\n");
+        printf("%d",st.st_mtime)
         break;
 
     case 'a':
+        printf("You choose to print the access rights of the regular file\n");
+        printf("User:\n")
+        if(stat.st_mode & S_IRUSR){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWUSR){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXUSR){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
+
+        printf("Group:\n")
+        if(stat.st_mode & S_IRGRP){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWGRP){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXGRP){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
+
+        printf("Other:\n")
+        if(stat.st_mode & S_S_IROTH){
+            printf("Read-yes\n");
+        }
+        else{
+            printf("Read-no\n");
+        }
+        if(stat.st_mode & S_IWOTH){
+            printf("Write-yes\n");
+        }
+        else{
+            printf("Write-no\n");
+        }
+        if(stat.st_mode & S_IXOTH){
+            printf("Execute-yes\n");
+        }
+        else{
+            printf("Execute-no\n");
+        }
         break;
 
     case 'l':
+        printf("You choose to create a symbolic link of the regular file\n");
+        const char *new_path;
+        symlink(path, new_path);
         break;
 
     default:
@@ -47,16 +215,16 @@ int main(int argc, char* argv[]){
             continue;
         }
         if(S_ISREG(st.st_mode)){
-            printf("%s is a regular file. \n", path);
-            //print_menu(path);
+            printf("%s is a regular file. \n", path, st);
+            print_menu_regfile(path, st);
         }
         if(S_ISDIR(st.st_mode)){
-            printf("%s is a directory. \n", path);
-            //print_menu(path,2);
+            printf("%s is a directory. \n", path, st);
+            //print_menu_dir(path,st);
         }
         if(S_ISLNK(st.st_mode)){
-            printf("%s is a symbolic link. \n", path);
-            //print_menu(path,3);
+            printf("%s is a symbolic link. \n", path, st);
+            //print_menu_link(path, st);
         }
     }
     return 0;
